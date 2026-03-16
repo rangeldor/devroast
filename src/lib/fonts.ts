08469527c295cb246bb2faded797bb3f680b67b8
Daniel <daniel.oliveira@labsecreto.com>
2026-03-16 13:49:03 -0300

    fix: load fonts using fs instead of imports

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,26 +1,34 @@
-import GeistRegular from "@/fonts/Geist-Regular.woff2";
-import JetBrainsMonoBold from "@/fonts/JetBrainsMono-Bold.woff2";
-import JetBrainsMonoRegular from "@/fonts/JetBrainsMono-Regular.woff2";
+import { readFile } from "fs/promises";
+import { join } from "path";
 
 export async function getOgFonts() {
+	const basePath = join(process.cwd(), "src", "fonts");
+
+	const [jetbrainsMonoRegular, jetbrainsMonoBold, geistRegular] =
+		await Promise.all([
+			readFile(join(basePath, "JetBrainsMono-Regular.woff2")),
+			readFile(join(basePath, "JetBrainsMono-Bold.woff2")),
+			readFile(join(basePath, "Geist-Regular.woff2")),
+		]);
+
 	return [
 		{
 			name: "JetBrains Mono",
-			data: JetBrainsMonoRegular,
+			data: jetbrainsMonoRegular.buffer,
 			weight: 400,
 			style: "normal",
 		},
 		{
 			name: "JetBrains Mono",
-			data: JetBrainsMonoBold,
+			data: jetbrainsMonoBold.buffer,
 			weight: 700,
 			style: "normal",
 		},
 		{
 			name: "Geist",
-			data: GeistRegular,
+			data: geistRegular.buffer,
 			weight: 400,
 			style: "normal",
 		},
-	];
+	] as const;
 }
